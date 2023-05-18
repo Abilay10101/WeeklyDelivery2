e1d6049b60e582f93194044fbdefc1f498460138
Abilay10101 <60212530+Abilay10101@users.noreply.github.com>
2023-05-18 19:58:17 +0600

    Initial commit

---
+++ WeeklyDelivery2/WeeklyDelivery2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyDelivery2App.swift
+//  WeeklyDelivery2
+//
+//  Created by Arip Khozhbanov on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyDelivery2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
